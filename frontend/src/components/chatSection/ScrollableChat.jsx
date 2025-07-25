c0ca40219d81d57e6ff52b268be3ed6f377ee239
Sachhyam Pokharel <pokharelsachhyam@gmail.com>
2025-07-25 18:16:43 +0545

    Scroll hidden

--- frontend/src/components/chatSection/ScrollableChat.jsx
+++ frontend/src/components/chatSection/ScrollableChat.jsx
@@ -20,54 +20,60 @@ const ScrollableChat = ({ messages }) => {
       display={"flex"}
       flexDirection={"column"}
       flex="1"
-      justifyContent={"flex-end"}
       overflowY="auto"
       paddingX={3}
       paddingY={1}
       maxHeight="100%"
       className="hide-scrollbar"
     >
-      {messages &&
-        messages.map((msg, i) => (
-          <div style={{ display: "flex" }} key={msg._id}>
-            {(isSameSender(messages, msg, i, userId) ||
-              isLastMessage(messages, i, userId)) && (
-              <CTooltip.Root label={msg.sender.name} hasArrow>
-                <CAvatar.Root
-                  mt="7px"
-                  mr={1}
-                  size="sm"
-                  cursor="pointer"
-                  src={msg.sender.avatar}
-                  name={msg.sender.name}
-                >
-                  <CAvatar.Image
+      <Box
+        display="flex"
+        flexDirection="column"
+        justifyContent="flex-end"
+        flex="1"
+      >
+        {messages &&
+          messages.map((msg, i) => (
+            <div style={{ display: "flex" }} key={msg._id}>
+              {(isSameSender(messages, msg, i, userId) ||
+                isLastMessage(messages, i, userId)) && (
+                <CTooltip.Root label={msg.sender.name} hasArrow>
+                  <CAvatar.Root
+                    mt="7px"
+                    mr={1}
+                    size="sm"
+                    cursor="pointer"
                     src={msg.sender.avatar}
                     name={msg.sender.name}
-                  />
-                </CAvatar.Root>
-              </CTooltip.Root>
-            )}
-            <Box display="flex" width="100%">
-              <span
-                style={{
-                  backgroundColor: `${
-                    msg.sender._id === userId ? "#95A8E0" : "teal"
-                  }`,
-                  color: "black",
-                  borderRadius: "20px",
-                  padding: "3px 10px",
-                  maxWidth: "75%",
-                  marginLeft: isSameSenderMargin(messages, msg, i, userId),
-                  marginTop: isSameSender(messages, msg, i, userId) ? 3 : 8,
-                }}
-              >
-                {msg.content}
-              </span>
-            </Box>
-          </div>
-        ))}
-      <div ref={bottomRef} />
+                  >
+                    <CAvatar.Image
+                      src={msg.sender.avatar}
+                      name={msg.sender.name}
+                    />
+                  </CAvatar.Root>
+                </CTooltip.Root>
+              )}
+              <Box display="flex" width="100%">
+                <span
+                  style={{
+                    backgroundColor: `${
+                      msg.sender._id === userId ? "#95A8E0" : "teal"
+                    }`,
+                    color: "black",
+                    borderRadius: "20px",
+                    padding: "3px 10px",
+                    maxWidth: "75%",
+                    marginLeft: isSameSenderMargin(messages, msg, i, userId),
+                    marginTop: isSameSender(messages, msg, i, userId) ? 3 : 8,
+                  }}
+                >
+                  {msg.content}
+                </span>
+              </Box>
+            </div>
+          ))}
+        <div ref={bottomRef} />
+      </Box>
     </Box>
   );
 };
