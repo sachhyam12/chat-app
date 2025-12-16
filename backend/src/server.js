d34490b1ac279b5fcdf2a0716af565625b84f73c
Sachhyam Pokharel <pokharelsachhyam@gmail.com>
2025-12-16 15:13:35 +0545

    into deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -15,7 +15,7 @@ connectDB()
     const io = new Server(server, {
       pingTimeout: 60000,
       cors: {
-        origin: "http://localhost:5173",
+        origin: `${process.env.FRONTEND_URL}`,
       },
     });
     io.on("connection", (socket) => {
