d1ee1008edbdfe5730e4af34c39b67aeece8f332
Sachhyam Pokharel <pokharelsachhyam@gmail.com>
2025-12-16 16:04:20 +0545

    cors-allow-origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json({ limit: "16kb" }));
 app.use(
   cors({
-    origin: `${FRONTEND_URL}` || "http://localhost:5173",
+    origin: `${process.env.FRONTEND_URL}` || "http://localhost:5173",
     credentials: true,
   })
 );
